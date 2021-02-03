f04ccb13aa1735a220d30611eed9ee2b558a714b
jskyler <72110869+jskyler@users.noreply.github.com>
2021-02-02 21:16:49 -0600

    save point

--- static/js/app.js
+++ static/js/app.js
@@ -3,12 +3,16 @@ d3.json("samples.json").then((importedData) => {
 
     var ids = importedData.samples[0].otu_ids;
     console.log(ids)
+    
     var sampleValues  = importedData.samples[0].sample_values.slice(0,10).reverse();
     console.log(sampleValues)
+    
     var otuLabels = importedData.samples[0].otu_labels.slice(0,10).reverse();
     console.log(otuLabels)
+    
     var top_otu = importedData.samples[0].otu_ids.slice(0,10).reverse();
     console.log(top_otu)
+
     var id_otu = top_otu.map(d => "OTU " + d);
     console.log(`OTU IDs: ${id_otu}`)
 
