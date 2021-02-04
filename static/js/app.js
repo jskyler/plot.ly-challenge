be1d4cb04bbbdbae8a341ab0b85448585bf573d6
jskyler <72110869+jskyler@users.noreply.github.com>
2021-02-03 18:17:13 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,86 +1,84 @@
-function getPlots(id) {
-console.log('getPlots');
-    d3.json("samples.json").then((importedData) => {
-        console.log('Hi' + importedData);
+function optionChanged(index) {
+    d3.json("samples.json").then(function (data) {
 
-        var ids = importedData.samples[0].otu_ids;
-        console.log(ids)
-        
-        var sampleValues  = importedData.samples[0].sample_values.slice(0,10).reverse();
-        console.log(sampleValues)
-        
-        var otuLabels = importedData.samples[0].otu_labels.slice(0,10).reverse();
-        console.log(otuLabels)
-        
-        var top_otu = importedData.samples[0].otu_ids.slice(0,10).reverse();
-        console.log(top_otu)
+        var filteredData = data.samples.filter(row => row.id == index);
+        console.log(filteredData);
 
-        var id_otu = top_otu.map(d => "OTU " + d);
-        console.log(`OTU IDs: ${id_otu}`)
+        getPlots(filteredData[0])
 
-        var trace1 = {
-            x: sampleValues,
-            y: id_otu,
-            text: otuLabels,
-            type: "bar",
-            orientation: "h"
-        };
-
-        var data1 = [trace1];
-        console.log(data1);
-        var layout = {
-            title: "Top 10 OTUs"
-        }
-
-        Plotly.newPlot("bar", data1, layout);
-
-        var trace2 = {
-            x: ids,
-            y: sample_values,
-            mode: "markers",
-            
-            marker: {
-                size: sample_values,
-                color: otu_ids,
-                colorscale: 'Earth'}
-        };
-
-        var layout2 = {
-            xaxis: {title: "OTU ID"},
-            height: 600,
-            width: 1000
-        };
-
-        var data2 = [trace2];
-
-        Plotly.newPlot("bubble", data2, layout2);
-    
+        var panelData = data.metadata.filter(row => row.id == index);
+        getDemos(panelData[0]);
     });
+ 
 }
 
-function getDemos(id) {
+function getDemos(plotData) {
     d3.json("samples.json").then((demos) => {
 
-        var metadata = demos.metadata;
-        console.log(metadata)
-
-        var results = metadata.filter(meta => meta.id.toString() === id)[0];
-
+        console.log(plotData)
         var demoDetails = d3.select("#sample-metadata");
 
         demoDetails.html("");
 
-        Object.entries(results).forEach((key) => {
-            demoDetails.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n")
+        Object.entries(plotData).forEach(([keys, value])=> {
+            demoDetails.append("h5").text(`${keys} : ${value}`)
         });
 
     });
 }
 
-function optionChanged(id) {
-    getPlots(id);
-    getDemos(id);
+function getPlots(plotData) {
+console.log('getPlots');
+    console.log('Hi' + plotData);
+
+    var sampleValues  = plotData.sample_values.slice(0,10);
+    console.log(sampleValues);
+    var otuLabels = plotData.otu_labels.slice(0,10);
+    console.log(otuLabels);
+    var otuIds = plotData.otu_ids.slice(0,10);
+    console.log(otuIds)
+
+    var trace1 = {
+        x: sampleValues.reverse(),
+        y: otuIds.map(object => "OTU " + object).reverse(),
+        text: otuLabels.reverse(),
+        type: "bar",
+        orientation: "h"
+    };
+
+    var data1 = [trace1];
+
+    var layout = {
+        title: "Top 10 OTUs"
+    }
+
+    Plotly.newPlot("bar", data1, layout);
+
+    var trace2 = {
+        x: otuIds,
+        y: sampleValues,
+        text: otuLabels,
+        mode: "markers",
+        
+        marker: {
+            size: sampleValues,
+            color: otuIds,
+            colorscale: 'Earth'}
+    };
+
+    var data2 = [trace2];
+
+    var layout2 = {
+        xaxis: {title: "OTU ID"},
+        height: 600,
+        width: 1000
+    };
+
+    Plotly.newPlot("bubble", data2, layout2);
+    
 }
+
+
 
 function init() {
 
@@ -90,11 +88,16 @@ function init() {
         console.log(data)
 
         data.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
+            dropdown.append("option").text(name).property("value",name);
         });
 
-        getPlots(data.names[0]);
-        getDemos(data.names[0]);
+        var startData = data.samples[0]
+        console.log(data);
+        getPlots(startData);
+        getDemos(startData);
+
+        var metadata = data.metadata[0];
+        getDemos(metadata);
 
     });
 }
